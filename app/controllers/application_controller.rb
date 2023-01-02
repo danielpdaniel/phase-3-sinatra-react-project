ff69de4ad47bc6148c49c8bfb9ca0d99a7ef2506
danielpdaniel <artofjquinn@gmail.com>
2023-01-02 18:08:29 -0500

    defines some get and post routes for the three tables in the api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,13 +16,38 @@ class ApplicationController < Sinatra::Base
     artists.to_json
   end
 
+  post "/artists" do 
+    new_artist = Artist.create(
+      name: params[:name]
+    )
+    new_artist.to_json
+  end
+
   get "/songs" do
     songs = Song.all
     songs.to_json
   end
 
+  post "/songs" do
+    new_song = Song.create(
+      title: params[:title],
+      artist_id: params[:artist_id]
+    )
+
+    new_song.to_json
+  end
+
   get "/covers" do
     covers = Cover.all
     covers.to_json
+  end
+
+  post "/covers" do
+    new_cover = Cover.create(
+      song_id: params[:song_id],
+      artist_id: params[:artist_id],
+      performance_link: params[:performance_link]
+    )
+    new_cover.to_json
   end
 end
