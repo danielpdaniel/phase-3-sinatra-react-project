043ad3b9a7d8453deee41a9bc3310deee223f37f
danielpdaniel <artofjquinn@gmail.com>
2023-01-06 14:57:12 -0500

    includes :song with :covers in /artists GET route handler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < Sinatra::Base
 
   get "/artists" do
     artists = Artist.all
-    artists.to_json(include: [:songs, :covers])
+    artists.to_json(include: [:songs, covers: {include: [:song]}])
   end
 
   get"/artists/:id" do
