c1d5eb0968604de40ca9c54ef087b65082e3df74
danielpdaniel <artofjquinn@gmail.com>
2023-01-06 13:12:28 -0500

    removes example get request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
-
+  
   get "/test" do
     tests = TestData.all
     tests.to_json
